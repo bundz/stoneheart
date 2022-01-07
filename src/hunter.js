e0da084a1d2fd4640a5c59e5b53cbdfc8a939c36
Murilo Uarth <murastuarth@gmail.com>
2022-01-07 19:28:04 -0300

    added hunter class

--- src/hunter.js
+++ src/hunter.js
@@ -1,7 +1,12 @@
 const Player = require('./player');
 
 class Hunter extends Player {
-
+  ability() {
+    this.oponent.suffer(2);
+    if(this.oponent.isDead()) {
+      finish();
+    }
+  }
 };
 
 module.exports = Hunter;
