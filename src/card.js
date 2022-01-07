522ae1414d6aee50aa2f1da9fa022507927f3e3e
Johnny <johnnyf.birck@gmail.com>
2022-01-07 18:11:54 -0300

    Creating all card methods

--- src/card.js
+++ src/card.js
@@ -1,4 +1,26 @@
-class Card {
+class Card{
+    constructor(id, name, cardClass, manaCost){
+        this.id = id;
+        this.name = name;
+        this.cardClass = cardClass;
+        this.manaCost = manaCost;
+    };
+
+    getNome(){
+        return this.name;
+    };
+
+    getCardClass(){
+        return this.cardClass;
+    };
+
+    getManaCost(){
+        return this.manaCost;
+    };
+
+    getId(){
+        return this.id;
+    };
 
 };
 
