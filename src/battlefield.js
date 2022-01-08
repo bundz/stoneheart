86f64279f6a522959b9a27992131896d0e20af32
Nicolas Sosa Machado <nicolassosamachado25@gmail.com>
2022-01-07 21:09:42 -0300

    Creating all card methods

--- src/battlefield.js
+++ src/battlefield.js
@@ -1,4 +1,22 @@
+const Graveyard = require('./graveyard');
+const MinionCard = require('./minionCard');
+
 class Battlefield {
+  constructor(capacity){
+    this.minions = [];
+    this.capacity = 7;
+  }
+  removeCard(index){
+    for (let i = 0; i < this.minions.length; i++){
+      if (this.minions[i].id == index){
+        Graveyard.addCard(minions[i]);
+        this.minions.splice(i,1);
+      }
+    }
+  }
+  addCard(card){
+    this.minions.push[card];
+  }
 
 };
 
