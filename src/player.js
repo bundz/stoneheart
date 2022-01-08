21e760140638a210a26c4d048000539f4bf77bf8
Nicolas Sosa Machado <nicolassosamachado25@gmail.com>
2022-01-07 21:11:57 -0300

    Created Attributes

--- src/player.js
+++ src/player.js
@@ -1,5 +1,49 @@
-class Player {
+const Card = require("./card");
+const Deck = require("./deck");
+const Battlefield = require("./battlefield");
+const Graveyard = require("./graveyard");
+const Match = require("./match");
 
+class Player {
+  constructor (id, name, totalMana, currentMana, playerClass){
+    this.id = id;
+    this.name = name;
+    this.life = 30;
+    this.totalMana = totalMana;
+    this.currentMana = currentMana;
+    this.deck = Deck;
+    this.battlefield = Battlefield;
+    this.graveyard = Graveyard;
+    this.playerClass = playerClass;
+    this.match = Match;
+    this.hand = [Card, Card, Card];
+    this.oponent = Player;
+  }
+  passTurn(){
+
+  }
+  castCard(cardIndex){
+
+  }
+  drawCard(){
+
+  }
+  classAbility(){
+
+  }
+  attack(){
+
+  }
+  attack(attacker,target){
+
+
+  }
+  suffer(damage){
+
+  }
+  isDead(){
+    
+  }
 };
 
 module.exports = Player;
