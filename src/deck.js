fbea69acaa2518e94739ba392812f06aa3d3549c
Murilo Uarth <murastuarth@gmail.com>
2022-01-11 16:39:26 -0300

    Deck with isEmpty() and getLength()

--- src/deck.js
+++ src/deck.js
@@ -1,5 +1,24 @@
 class Deck {
-  shuffleDeck() {
+  constructor(cards) {
+    this.cards = [];
+  }
+
+  isEmpty() {
+    if(this.cards.length() === 0) {
+      return true;
+    }
+    return false;
+  }
+
+  getLength() {
+    return this.cards.length;
+  }
+
+  shuffle() {
+
+  }
+
+  pop() {
     
   }
 };
