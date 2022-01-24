e3e651ce7e7bb613098568504a9f2ebd0306c582
nicolas <nicolassosamachado25@gmail.com>
2022-01-24 17:05:59 -0300

    Test graveyard.js

---
+++ test/graveyard.test.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai');
+const Graveyard = require("../src/graveyard");
+const cardsData = require('../database/cards.json');
+const Card = require('../src/card');
+const CardLoader = require('../src/cardLoader');
+
+describe('Graveyard', () => {
+
+  let cardLoader;
+  let cardsDeck;
+  const graveyard = new Graveyard();
+
+  beforeEach(() => {
+    cardLoader = new CardLoader(cardsData);
+    cardsDeck = cardLoader.generateCards([1]);
+  })
+
+  context('addCard',() => {
+    it('should add card in graveyard', () => {
+      let card = cardsDeck[0];
+      graveyard.addCard(card);
+      expect(graveyard.cards.length).to.not.equal(0);
+      expect(graveyard.cards[0]).to.be.instanceOf(Card);
+    })
+  });
+
+})
