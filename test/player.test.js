574a48910cb4c09c5b4d118b844aab6b7f1f12b2
Johnny <johnnyf.birck@gmail.com>
2022-01-26 15:12:35 -0300

    created the player test (except for the attack)

--- test/player.test.js
+++ test/player.test.js
@@ -10,13 +10,11 @@ const Match = require("../src/match");
 const Mage = require("../src/mage");
 const Hunter = require("../src/hunter");
 
-describe.only('Player', () => {
+describe('Player', () => {
 
     let cardLoader;
     let cards;
     let deck;
-    let battlefield;
-    let graveyard;
     let mage;
     let hunter;
     
@@ -67,7 +65,9 @@ describe.only('Player', () => {
         it('Should change the atual player', () => {
             const match = new Match(mage, hunter);
             mage.match = match;
+            mage.oponent = hunter;
             match.currentPlayer = mage;
+            hunter.addTotalMana(1);
             mage.passTurn();
             expect(match.currentPlayer).to.be.equal(mage.oponent)
         })
@@ -81,6 +81,77 @@ describe.only('Player', () => {
             
             expect(mage.totalMana).to.be.equal(mana+3);
             expect(mage.totalMana).to.be.below(11)
+        })
+    })
+
+    context('refreshMana', () => {
+        it('Should make the currentMana the same as totalMana', () => {
+            mage.currentMana = 1;
+            mage.totalMana = 3;
+            
+            mage.refreshMana();
+            
+            expect(mage.currentMana).to.be.equal(mage.totalMana);
+        })
+    })
+    
+    context('drawCard', () => {
+        it('Should remove a card from deck and add it to hand', () => {
+            let hand_length = mage.hand.length;
+            let deck_length = mage.deck.getLength();
+            mage.drawCard();
+            
+            expect(mage.hand.length).to.be.equal(hand_length+1);
+            expect(mage.deck.getLength()).to.be.equal(deck_length-1);
+        })
+    })
+
+    context('suffer', () => {
+        it('Should damage the player', () => {
+            let life = mage.life;
+            mage.suffer(1);
+            expect(mage.life).to.be.equal(life-1);
+        })
+
+        it('Should finish the match if someone die', () => {
+            
+            const match = new Match(mage, hunter);
+            mage.match = match;
+            mage.life = 1
+            mage.suffer(1);
+            expect(mage.match.state).to.be.equal("finished");
+        })
+    })
+
+    context('isDead', () => {
+        it('Should return false if players life is above zero', () => {
+            expect(mage.isDead()).to.be.false
+        })
+        it('Should return true if players life is under one', () => {
+            mage.life = 0
+            expect(mage.isDead()).to.be.true
+        })
+    })
+
+    context('suffleDeck', () => {
+        it('Should shuffle the player deck', () => {
+            let deck_length = mage.deck.getLength();
+            mage.shuffleDeck();
+            expect(mage.deck.getLength()).to.be.equals(deck_length)
+            for( card of mage.deck.cards){
+                expect(card).to.be.instanceOf(Card);
+            }
+        })
+    })
+
+    context('drawInitialCards', () => {
+        it('Should draw the players first hand', () => {
+            mage.drawInitialCards();
+            expect(mage.hand.length).to.be.equal(3);
+
+            for( card of mage.hand ){
+                expect(card).to.be.instanceOf(Card)
+            }
         })
     })
 })
